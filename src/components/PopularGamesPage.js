785fd0563f1e4d8742af1f93f65cd1695db1b954
Casterfeel <sanikovmaxim@mail.ru>
2024-11-29 14:22:41 +0300

    final commit

--- src/components/PopularGamesPage.js
+++ src/components/PopularGamesPage.js
@@ -1,83 +1,103 @@
-import React from 'react';
-import chev from '../assets/chev.png';
-import search from '../assets/ico-search.png';
-import arrow from '../assets/arrow.png';
-import game1 from '../assets/game1.png';
-import game2 from '../assets/game2.png';
-import game3 from '../assets/game3.png';
-import game4 from '../assets/game4.png';
-import game5 from '../assets/game5.png';
-import game6 from '../assets/game6.png';
-import game7 from '../assets/game7.png';
-import game8 from '../assets/game8.png';
-import game9 from '../assets/game9.png';
-import game10 from '../assets/game10.png';
-import game11 from '../assets/game11.png';
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import chev from "../assets/chev.png";
+import search from "../assets/ico-search.png";
+import arrow from "../assets/arrow.png";
+import game1 from "../assets/game1.png";
+import game2 from "../assets/game2.png";
+import game3 from "../assets/game3.png";
+import game4 from "../assets/game4.png";
+import game5 from "../assets/game5.png";
+import game6 from "../assets/game6.png";
+import game7 from "../assets/game7.png";
+import game8 from "../assets/game8.png";
+import game9 from "../assets/game9.png";
+import game10 from "../assets/game10.png";
+import game11 from "../assets/game11.png";
+import { useNavigate } from "react-router-dom";
+
+const initialGames = [
+  game1, game2, game3, game4, game5, game6, game7, game8, game9, game10, game11, game1
+];
+
+
+const additionalGames = [
+  game2, game3, game4, game5, game6, game7, game8, game9, game10, game11, game1, game2, game3, game4, game5, game6, game7, game8
+];
 
 const PopularGamesPage = () => {
+  
+  const [games, setGames] = useState(initialGames);
+  
+  const [gamesCount, setGamesCount] = useState('Displaying 12 of 30 games');
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    const navigate = useNavigate();
-    const handleChevClick = () => {
-      navigate('/');
-    }
+  
+  const handleLoadMoreClick = () => {
+    if (isLoaded) return;
+    setGames(prevGames => {
+      const newGames = [...prevGames, ...additionalGames]; 
+      return newGames;
+    });
+
+    
+    setGamesCount('Displaying 30 of 30 games');
+    setIsLoaded(true)
+  };
+
+  const navigate = useNavigate();
+
+  
+  const handleChevClick = () => {
+    navigate('/');
+  }
 
   return (
     <div className='popularGames'>
       <div className="popular__header">
         <img 
-        src= {chev} 
-        alt="chevron" 
-        onClick={handleChevClick}
+          src={chev} 
+          alt="chevron" 
+          onClick={handleChevClick}
         />
         <h1>Popular Games</h1>
       </div>
+
       <div className="popular__fields">
         <div className="popular__fields-top">
-            <div className="search__block">
-                <img src={search} alt="" />
-                <p>Search Game</p>
-            </div>
-            <div className="green__block">
-                <p>I’m Feeling Lucky</p>
-            </div>
+          <div className="search__block">
+            <img src={search} alt="search" />
+            <p>Search Game</p>
+          </div>
+          <div className="green__block">
+            <p>I’m Feeling Lucky</p>
+          </div>
         </div>
         <div className="popular__fields-bot">
-            <div className="field__left">
-                <p>Providers</p>
-                <img src={arrow} alt="arrow" />
-            </div>
-            <div className="field__right">
-                <p>A-Z</p>
-                <img src={arrow} alt="arrow" />
-            </div>
+          <div className="field__left">
+            <p>Providers</p>
+            <img src={arrow} alt="arrow" />
+          </div>
+          <div className="field__right">
+            <p>A-Z</p>
+            <img src={arrow} alt="arrow" />
+          </div>
         </div>
       </div>
+
       <div className="popular__games">
-        <div className="game__1">
-            <img src={game1} alt="game1" />
-            <img src={game2} alt="game2" />
-            <img src={game3} alt="game3" />
-        </div>
-        <div className="game__2">
-            <img src={game4} alt="game4" />
-            <img src={game5} alt="game5" />
-            <img src={game6} alt="game6" />
-        </div>
-        <div className="game__3">
-            <img src={game6} alt="game6" />
-            <img src={game7} alt="game7" />
-            <img src={game8} alt="game8" />
-        </div>
-        <div className="game__4">
-            <img src={game9} alt="game9" />
-            <img src={game10} alt="game10" />
-            <img src={game11} alt="game11" />
-        </div>
+        
+        {Array.from({ length: Math.ceil(games.length / 3) }).map((_, index) => (
+          <div className={`game__row`} key={index}>
+            {games.slice(index * 3, (index + 1) * 3).map((game, i) => (
+              <img key={i} src={game} alt={`game${index * 3 + i + 1}`} />
+            ))}
+          </div>
+        ))}
       </div>
+
       <div className="games__button-block">
-        <button>Load More</button>
-        <p>Displaying 12 of 30 games</p>
+        <button onClick={handleLoadMoreClick}>Load More</button>
+        <p>{gamesCount}</p>
       </div>
     </div>
   );
